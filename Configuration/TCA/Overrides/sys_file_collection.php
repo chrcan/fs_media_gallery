a9d9b32ae465fab606fa30ecd82511a98b9f3cf5
Eric Harrer <info@eric-harrer.de>
2025-07-21 10:33:11 +0200

    [BUGFIX] Fix broken configuration of field sys_file_collection.main_asset

--- Configuration/TCA/Overrides/sys_file_collection.php
+++ Configuration/TCA/Overrides/sys_file_collection.php
@@ -65,29 +65,15 @@ $additionalColumns = [
     'main_asset' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:fs_media_gallery/Resources/Private/Language/locallang_db.xlf:tx_fsmediagallery_domain_model_mediaalbum.main_asset',
-        // 'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
-        //     'images',
-        //     [
-        //         'appearance' => [
-        //             'createNewRelationLinkTitle' => 'LLL:EXT:fs_media_gallery/Resources/Private/Language/locallang_db.xlf:tx_fsmediagallery_domain_model_mediaalbum.main_asset.add'
-        //         ],
-        //         'maxitems' => 1,
-        //     ],
-        //     $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
-
-        'images' => [
-            'config' => [
-                'appearance' => [
-                    'createNewRelationLinkTitle' => 'LLL:EXT:fs_media_gallery/Resources/Private/Language/locallang_db.xlf:tx_fsmediagallery_domain_model_mediaalbum.main_asset.add',
-                ],
-                'type' => 'file',
-                'maxitems' => 1,
-                'allowed' => 'common-image-types',
+        'config' => [
+            'type' => 'file',
+            'allowed' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
+            'appearance' => [
+                'createNewRelationLinkTitle' => 'LLL:EXT:fs_media_gallery/Resources/Private/Language/locallang_db.xlf:tx_fsmediagallery_domain_model_mediaalbum.main_asset.add',
             ],
+            'maxitems' => 1,
         ],
     ],
-    // )
-    // ],
     'slug' => [
         'exclude' => true,
         'label' => 'LLL:EXT:fs_media_gallery/Resources/Private/Language/locallang_db.xlf:tx_fsmediagallery_domain_model_mediaalbum.slug',
